08fb1f5314f26eaa49426fc1300887775737166c
Shiva3366 <shiva@electems.com>
2025-11-15 11:00:34 +0530

    commit fix initial push

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import fs from "fs/promises";
+import fs from "fs/promises"; 
 import cors from "cors";
 
 const app = express();
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -9,6 +9,6 @@ createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-    </BrowserRouter>
+    </BrowserRouter> 
   </React.StrictMode>
 )
